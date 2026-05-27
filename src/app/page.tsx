144e5d553e57428cbe7551ae1f5d8cd3adc5eecb
Jokalot <gabriel.gastelum.f@gmail.com>
2026-05-26 23:26:21 -0700

    Guardar cambios antes de merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { FAQSection, CTASection } from '@/components/home/FAQandCTA';
 export default function Home() {
   return (
     <>
+
       <HeroSection />
       <TickerBar />
       <StatsRow />
